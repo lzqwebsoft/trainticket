b9fd6f16b5d49fabd91591277fe4a92bf6b959d1
Johnny Luo <lzqwebsoft@gmail.com>
2014-01-09 12:12:12 +0800

    update cx_Freeze setup script

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 # coding = utf-8
 from cx_Freeze import setup, Executable
 
+build_exe_options = {
+    'compressed': True,
+    'packages':['ui', 'core', 'common'],
+}
 setup(
-	name="trainticket",
-	version = '0.1',
-	description = '体验版',
-	executables=[Executable("access12306.py", icon='logo.ico')])
+    name="trainticket",
+    version='0.2.0',
+    description='风险版',
+    options={'build_exe': build_exe_options},
+    executables=[Executable("access12306.py", icon='logo.ico')])
